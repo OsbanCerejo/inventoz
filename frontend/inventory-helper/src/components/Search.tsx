e9cf8e70ff811fc2574c2c969c513fab6357f1a9
Osban Cerejo <osbanacerejo@gmail.com>
2024-05-20 22:49:29 -0400

    Changes to Togglable Nav Bar

--- frontend/inventory-helper/src/components/Search.tsx
+++ frontend/inventory-helper/src/components/Search.tsx
@@ -1,11 +1,75 @@
-import { useParams } from "react-router-dom";
+import { Box, TextField, Select, MenuItem, Button } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
 
-function Search() {
-  let { id } = useParams();
+interface SearchProps {
+  columnMap: Map<string, string>;
+  onSearch: (data: any) => void;
+}
+
+function Search({ columnMap, onSearch }: SearchProps) {
+  const [searchString, setSearchString] = useState("");
+  const [selectedColumn, setSelectedColumn] = useState("");
+
+  const handleSearch = async () => {
+    try {
+      if (searchString.trim() !== "") {
+        const searchType = columnMap.has(selectedColumn)
+          ? columnMap.get(selectedColumn)
+          : "itemName";
+        const response = await axios.get(
+          "http://localhost:3001/products/search",
+          {
+            params: { searchString, searchType },
+          }
+        );
+        onSearch(response.data);
+      }
+    } catch (error) {
+      console.error("Error searching products:", error);
+    }
+  };
+
+  // Function to handle search on pressing Enter key
+  const handleKeypress = (e: any) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      handleSearch();
+    }
+  };
+
   return (
     <div>
-      <p>Search item is </p>
-      {id}
+      <Box alignItems="center" my={4} p={2}>
+        <TextField
+          style={{ width: "85%" }}
+          label="Search Item Name"
+          id="search"
+          value={searchString}
+          onChange={(event) => {
+            setSearchString(event.target.value);
+          }}
+          onKeyDown={handleKeypress}
+        />
+        <Select
+          value={selectedColumn}
+          onChange={(event) => setSelectedColumn(event.target.value)}
+          displayEmpty
+          style={{ width: "15%" }}
+        >
+          <MenuItem value="" disabled>
+            Search By
+          </MenuItem>
+          {Array.from(columnMap.keys()).map((column, index) => (
+            <MenuItem key={index} value={column}>
+              {column}
+            </MenuItem>
+          ))}
+        </Select>
+        <Button variant="contained" color="success" onClick={handleSearch}>
+          Search
+        </Button>
+      </Box>
     </div>
   );
 }
