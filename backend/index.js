21536d295223562f56d65828aa1a6ecdf0fa5fb7
Shakil Anwar <38348848+shakilanwar1998@users.noreply.github.com>
2026-03-04 04:21:38 +0600

    Revert "Update index.js"
    This reverts commit 2b420f1006094989667c7ad396811a3e924ac235.

--- backend/index.js
+++ backend/index.js
@@ -97,9 +97,6 @@ app.use("/api/users", usersRouter);
 const barcodeScanRouter = require("./routes/BarcodeScan");
 app.use("/api/barcode-scan", barcodeScanRouter);
 
-const productVendorPricesRouter = require("./routes/ProductVendorPrices");
-app.use("/product-vendor-prices", productVendorPricesRouter);
-
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
